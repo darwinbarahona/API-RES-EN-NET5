214deee6c6a4ff94819e6cdf79d618d5c7c808da
darwinbarahona <danilozacarias10@gmail.com>
2021-10-13 21:09:22 -0600

    primer_comit

---
+++ web_api_db/Controllers/EmpleadosController.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using web_api_db.Models;
+namespace web_api_db.Controllers{
+     [Route("api/[controller]")]
+    public class EmpleadosController : Controller{
+        private Conexion dbConexion;
+        public EmpleadosController(){
+            dbConexion = Conectar.Create();
+        }
+        [HttpGet]
+        public ActionResult Get(){
+            return Ok (dbConexion.Clientes.ToArray());
+        }
+        [HttpGet("id")]
+        public async Task<ActionResult> Get(int id){
+            var empleados = await dbConexion.Empleados.FindAsync(id);
+            if (empleados !=null){
+                return Ok(empleados);
+
+            }else{
+                return NotFound();
+            }
+        }
+         [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Empleados empleados){
+            if (ModelState.IsValid){
+                dbConexion.Empleados.Add(empleados);
+                await dbConexion.SaveChangesAsync();
+                return Ok(empleados);
+            }else{
+                return NotFound();
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] Empleados empleados){
+            var v_empleados = dbConexion.Empleados.SingleOrDefault(a => a.id_empleados == empleados.id_empleados);
+            if (v_empleados !=null && ModelState.IsValid){
+                    dbConexion.Entry(v_empleados).CurrentValues.SetValues(empleados);
+                    await dbConexion.SaveChangesAsync();
+                    return Ok();
+            }else{
+                return NotFound();
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id){
+            var empleados = dbConexion.Empleados.SingleOrDefault(a => a.id_empleados == id);
+            if (empleados !=null){
+                dbConexion.Empleados.Remove(empleados);
+                await dbConexion.SaveChangesAsync();
+                return Ok();
+            }else{
+                return NotFound();
+            }
+        }
+    }
+}
